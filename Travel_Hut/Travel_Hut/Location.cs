efa73cda02d7a3b767ac2da2c4a085cc4853fa5a
ruizclar <77371302+ruizclar@users.noreply.github.com>
2021-09-09 21:32:16 -0500

    Module 2

---
+++ Travel_Hut/Travel_Hut/Location.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel_Hut
+{
+    public class Location: iAlllocations
+    {
+       //Variables for Location Class 
+       public int LocationID { get; set; }
+       public string LocationName { get; set; }
+       public string Address { get; set; }
+       public string City { get; set; }
+       public int Zip { get; set; }
+
+
+        //Default Constructor
+        public Location()
+        {
+            LocationID = 0;
+            LocationName = "unknown";
+            Address = "unknown";
+            City = "unkown";
+            Zip = 0;
+        }
+
+        //Non-default constructor 
+
+        public Location (int locationID, String locationName, String address, String city, int zip)
+        {
+            LocationID = locationID;
+            LocationName = locationName;
+            Address = address;
+            City = city;
+            Zip = zip;
+        }
+
+        public string listTravelInfo()
+        {
+            return this.LocationName + " is the best in " + this.City + ".";
+        }
+
+
+        public virtual void speak()
+        {
+
+        }
+
+    }
+
+    public interface iAlllocations
+    {
+       void speak();
+    }
+    public class Museum : Location
+    {
+
+       public override void speak()
+       {
+            
+       }
+    }
+
+}
+
+    
+  
---
+++ Travel_Hut/Travel_Hut/TravelHut.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Travel_Hut
+{
+    public partial class TravelHut : Form
+    {
+        Museum FieldMuseum = new Museum();
+
+        public TravelHut()
+        {
+            InitializeComponent();
+            //Museum FieldMuseum = new Museum();
+            FieldMuseum.LocationName = "The Field Museum";
+            FieldMuseum.City = "Chicago";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          this.displayTravelInfo.Text = FieldMuseum.listTravelInfo();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+          this.btnGetInfo.Click += new EventHandler(button1_Click);
+        }
+
+      
+    }
+}
